b899f8f8db03ebff3464de9270d90c9313982e35
Chris Suszyński <ksuszyns@redhat.com>
2023-04-16 00:57:55 +0200

    Proper main file for require

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import type { WasmRegistry, InvalidImage, InvalidWorkdir } from './registry'
+import type { WasmImage, Image, InvalidImageSpec } from './image'
+
+export {
+  Image,
+  WasmImage,
+  WasmRegistry,
+  
+  InvalidImageSpec,
+  InvalidImage,
+  InvalidWorkdir,
+}
